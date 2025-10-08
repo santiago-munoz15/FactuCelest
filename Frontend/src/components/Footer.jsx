a27b4303018adfa13e483df3e95569cc20c38d10
santiago-munoz15 <santiago.munoz15@correo.tdea.edu.co>
2025-10-07 23:11:27 -0500

    Frontend: vista de inicio de sesión lista

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full text-center py-4 bg-gray-300 text-gray-600 text-sm absolute bottom-0">
+      ® FactuCelest | Todos los derechos reservados
+    </footer>
+  );
+}
