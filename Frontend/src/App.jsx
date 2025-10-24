ef891de6a85518e29d1a1e8d54d1f715dba69eb3
santiago-munoz15 <santiago.munoz15@correo.tdea.edu.co>
2025-10-23 20:16:58 -0500

    Login temporal

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,35 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MainLayout from "./Layouts/MainLayout"; // 👈 importa el layout global
+import MainLayout from "./Layouts/MainLayout";
 import MenuPrincipal from "./pages/MenuPrincipal";
 import Productos from "./pages/Productos";
+import Login from "./pages/Login"; // 👈 importar login
 
 function App() {
   return (
     <Router>
-      <MainLayout>
-        <Routes>
-          <Route path="/" element={<MenuPrincipal />} />
-          <Route path="/productos" element={<Productos />} />
-        </Routes>
-      </MainLayout>
+      <Routes>
+        {/* Página de inicio (Login sin layout) */}
+        <Route path="/" element={<Login />} />
+
+        {/* Páginas dentro del layout principal */}
+        <Route
+          path="/menu"
+          element={
+            <MainLayout>
+              <MenuPrincipal />
+            </MainLayout>
+          }
+        />
+
+        <Route
+          path="/productos"
+          element={
+            <MainLayout>
+              <Productos />
+            </MainLayout>
+          }
+        />
+      </Routes>
     </Router>
   );
 }
